0825357df3f150542dfb6a6ed3012f4ecedd154c
wangliang <850598163@qq.com>
2020-05-29 19:05:58 +0800

    test

---
+++ projects/ngx-electron/build/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of build
+ */
+
+// export * from './lib/build.service';
+// export * from './lib/build.component';
+// export * from './lib/build.module';
+export * from './command';
+
+
